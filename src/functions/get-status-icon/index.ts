bc4d5a8862c7c2a89ecef45104a8c76e567afe7c
Ben Elferink <ben.elferink@icloud.com>
2025-02-07 23:59:36 +0200

    fix: remove useTheme from getStatusIcon

--- src/functions/get-status-icon/index.ts
+++ src/functions/get-status-icon/index.ts
@@ -1,10 +1,7 @@
 import { CheckCircledIcon, ErrorTriangleIcon, InfoIcon, OdigosLogo, type SVG, WarningTriangleIcon } from '@odigos/ui-icons'
-import { useTheme } from 'styled-components'
 import { NOTIFICATION_TYPE } from '../../@types'
 
-export const getStatusIcon = (type: NOTIFICATION_TYPE) => {
-  const theme = useTheme()
-
+export const getStatusIcon = (type: NOTIFICATION_TYPE, theme: Record<'text', Record<NOTIFICATION_TYPE, string>>) => {
   const LOGOS: Record<NOTIFICATION_TYPE, SVG> = {
     [NOTIFICATION_TYPE.SUCCESS]: (props) => CheckCircledIcon({ fill: theme.text[type], ...props }),
     [NOTIFICATION_TYPE.ERROR]: (props) => ErrorTriangleIcon({ fill: theme.text[type], ...props }),
